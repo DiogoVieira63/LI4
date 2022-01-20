da1fdf570e3ed1371b77821c1a7f31aa3e14fee3
Pedro Sousa <alex.pedro.sousa@gmail.com>
2022-01-20 17:30:32 +0000

    clean

--- HTTP/HTTPAutentication.java
+++ HTTP/HTTPAutentication.java
@@ -7,15 +7,17 @@ import javax.servlet.annotation.*;
 
 import src.GuideMeTo;
 
+@WebServlet(name = "HTTP.HTTPAutentication",value = "/aut")
+
 public class HTTPAutentication extends HttpServlet{
 
     private GuideMeTo gtm = new GuideMeTo();
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{ 
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
+        //String name = request.getParameter("name");
+        //String password = request.getParameter("password");
 
-        if (gtm.logIn(name, password)){ getServletContext().getRequestDispatcher("/p.hmtl").forward(request,response); }
-        else { getServletContext().getRequestDispatcher("/r.html").forward(request, response); }
+        if (gtm.logIn("eu", "tu")){ getServletContext().getRequestDispatcher("../p.hmtl").forward(request,response); }
+        else { getServletContext().getRequestDispatcher("../r.html").forward(request, response); }
     }
 }
